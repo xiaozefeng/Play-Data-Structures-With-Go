21e7a7976bef9e49b9d81628b7fca54b9dfdeb27
肖泽锋 <qq523107430@163.com>
2018-09-11 23:09:16 +0800

    动态数组和时间复杂度分析

---
+++ array/array.go
@@ -0,0 +1,131 @@
+package array
+
+import (
+	"errors"
+	"fmt"
+)
+
+type array struct {
+	data []int
+	size int
+}
+
+func (arr *array) String() string {
+	result := ""
+	result = fmt.Sprintf("Array: len:%d, size:%d ", len(arr.data), arr.size)
+	result += fmt.Sprintf(", Data: ")
+	for i := 0; i < arr.size; i++ {
+		result += fmt.Sprintf("%d ", arr.data[i])
+	}
+	return result
+}
+
+// the factory method
+func NewArrayWithCapacity(capacity int) *array {
+	return &array{
+		data: make([]int, capacity, capacity),
+		size: 0,
+	}
+}
+
+func NewArray() *array {
+	return NewArrayWithCapacity(10)
+}
+
+// 添加一个元素
+func (arr *array) Add(index, e int) (err error) {
+	if arr.size == len(arr.data) {
+		arr.resize(len(arr.data) * 2)
+	}
+	if index < 0 || index > arr.size {
+		return errors.New("index required >= 0 and <= size")
+	}
+	for i := arr.size; i > index; i-- {
+		arr.data[i] = arr.data[i-1]
+	}
+	arr.data[index] = e
+	arr.size ++
+	return nil
+}
+
+// 在尾部添加一个元素
+func (arr *array) AddLast(e int) error {
+	return arr.Add(arr.size, e)
+}
+
+//在头部添加一个元素
+func (arr *array) AddFirst(e int) error {
+	return arr.Add(0, e)
+}
+
+// 根据索引删除一个元素
+func (arr *array) Remove(index int) (e int, err error) {
+	if index < 0 || index >= arr.size {
+		return 0, errors.New("index required >= 0 and < size")
+	}
+	ret := arr.data[index]
+	for i := index; i < arr.size; i++ {
+		arr.data[i] = arr.data[i+1]
+	}
+	arr.size --
+	// 只有四分之一的空间在用的时候，减少容量
+	if len(arr.data)/4 == arr.size && len(arr.data)/2 != 0 {
+		arr.resize(len(arr.data) / 2)
+	}
+	return ret, nil
+
+}
+
+func (arr *array) RemoveFirst() (e int, err error) {
+	return arr.Remove(0)
+}
+
+func (arr *array) RemoveLast() (e int, err error) {
+	return arr.Remove(arr.size)
+}
+
+func (arr *array) Contains(e int) bool {
+	for _, val := range arr.data {
+		if val == e {
+			return true
+		}
+	}
+	return false
+}
+
+// 查找并删除第一个元素
+func (arr *array) RemoveElement(e int) bool {
+	index := arr.Find(e)
+	if index != -1 {
+		arr.Remove(index)
+		return true
+	}
+	return false
+}
+
+// 修改一个元素
+func (arr *array) Set(index, e int) (err error) {
+	if index < 0 || index >= arr.size {
+		return errors.New("index required >= 0 and <size")
+	}
+	arr.data[index] = e
+	return nil
+}
+
+// 根据元素查询索引
+func (arr *array) Find(e int) (index int) {
+	for index, val := range arr.data {
+		if val == e {
+			return index
+		}
+	}
+	return -1
+}
+
+func (arr *array) resize(capacity int) {
+	newData := make([]int, capacity, capacity)
+	for i := 0; i < arr.size; i++ {
+		newData[i] = arr.data[i]
+	}
+	arr.data = newData
+}
---
+++ array/array_test.go
@@ -0,0 +1,28 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	arr := NewArray()
+	err := arr.Add(0, 1)
+	if err != nil {
+		panic(err)
+	}
+	arr.Add(1,1)
+	arr.Add(0,2)
+	arr.Add(2,3)
+	fmt.Printf("%s\n", arr)
+	// 2131
+	e, er := arr.Remove(0)
+	if er != nil {
+		panic(er)
+	}
+	fmt.Printf("被删除的元素:%d\n", e)
+	fmt.Println(arr)
+
+
+}
+
