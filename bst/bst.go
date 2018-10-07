60f6bca8e820a33ce2536f37ff0e1190b11679f0
肖泽锋 <qq523107430@163.com>
2018-10-07 23:41:28 +0800

    bst

--- bst/bst.go
+++ bst/bst.go
@@ -15,6 +15,77 @@ func newNode(val int) *node {
 	}
 }
 
+type stack struct {
+	data []*node
+	size int
+}
+
+func (s *stack) getSize() int {
+	return s.size
+}
+
+func newStack() *stack {
+	return &stack{
+		size: 0,
+		data: make([]*node, 0),
+	}
+}
+
+func (s *stack) isEmpty() bool {
+	return s.size == 0
+}
+
+func (s *stack) push(node *node) {
+	s.data = append(s.data, node)
+	s.size++
+}
+
+func (s *stack) pop() *node {
+	if s.isEmpty() {
+		panic("pop failed. stack is empty")
+	}
+	stackLen := len(s.data)
+	ret := s.data[stackLen-1]
+	s.data = s.data[:stackLen-1]
+	s.size--
+	return ret
+}
+
+type queue struct {
+	data []*node
+	size int
+}
+
+func (q *queue) getSize() int {
+	return q.size
+}
+
+func (q *queue) isEmpty() bool {
+	return q.size == 0
+}
+
+func (q *queue) enQueue(node *node) {
+	q.data = append(q.data, node)
+	q.size++
+}
+
+func (q *queue) deQueue() *node {
+	if q.isEmpty() {
+		panic("deQueue failed. queue is empty")
+	}
+	ret := q.data[0]
+	q.data = q.data[1:]
+	q.size--
+	return ret
+}
+
+func newQueue() *queue {
+	return &queue{
+		data: make([]*node, 0),
+		size: 0,
+	}
+}
+
 type BST struct {
 	root *node
 	size int
@@ -87,6 +158,24 @@ func (b *BST) PreOrder() {
 	b.traverse(b.root)
 }
 
+// 非递归实现二分搜索树的前序遍历
+func (b *BST) PreOrderNR() {
+	stack := newStack()
+	stack.push(b.root)
+	for !stack.isEmpty() {
+		cur := stack.pop()
+		fmt.Printf("%d, ", cur.val)
+		if cur.right != nil {
+			stack.push(cur.right)
+		}
+
+		if cur.left != nil {
+			stack.push(cur.left)
+		}
+	}
+}
+
+// 函数式编程
 func (b *BST) traverseFunc(n *node, f func(n *node)) {
 	if n == nil {
 		return
@@ -96,6 +185,7 @@ func (b *BST) traverseFunc(n *node, f func(n *node)) {
 	b.traverseFunc(n.right, f)
 }
 
+// 前序遍历递归
 func (b *BST) traverse(n *node) {
 	if n == nil {
 		return
@@ -103,4 +193,19 @@ func (b *BST) traverse(n *node) {
 	fmt.Printf("%d, ", n.val)
 	b.traverse(n.left)
 	b.traverse(n.right)
+}
+
+func (b *BST) LevelOrder() {
+	queue := newQueue()
+	queue.enQueue(b.root)
+	for !queue.isEmpty() {
+		cur := queue.deQueue()
+		fmt.Printf("%d, ", cur.val)
+		if cur.left != nil {
+			queue.enQueue(cur.left)
+		}
+		if cur.right != nil {
+			queue.enQueue(cur.right)
+		}
+	}
 }
--- bst/bst_test.go
+++ bst/bst_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestBST(t *testing.T) {
 	bst := newBST()
 	numbers := []int{5, 3, 6, 8, 4, 2}
 	for _, val := range numbers {
@@ -13,4 +13,15 @@ func TestAdd(t *testing.T) {
 	}
 	fmt.Println("中序遍历:")
 	fmt.Println(bst)
+
+	fmt.Println("递归前序遍历")
+	bst.PreOrder()
+	fmt.Println()
+
+	fmt.Println("非递归前序遍历")
+	bst.PreOrderNR()
+
+	fmt.Println()
+	fmt.Println("广度优先遍历二分搜索树")
+	bst.LevelOrder()
 }
